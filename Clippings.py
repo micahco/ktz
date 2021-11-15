4b53310b3cb92032aa82326ad227fdeda52b1db7
micahco <micahcowell@gmail.com>
2021-11-15 10:54:51 -0800

    fixed unicode decode error

--- Clippings.py
+++ Clippings.py
@@ -16,7 +16,6 @@ for i, input in enumerate(litData):
         easygui.msgbox('REQUIRED: ' + reqData[i], 'ERROR')
         exit()
 
-
 # user vars
 CLIPPING_AUTHOR_LAST = litData[0]
 CLIPPING_AUTHOR_FIRST = litData[1]
@@ -25,6 +24,8 @@ CLIPPING_DIR = str(CLIPPING_AUTHOR_LAST + CLIPPING_YEAR) # i.e. Williams1937
 CLIPPING_PATH = '../../literature/' + CLIPPING_DIR # path to new literature folder
 TEMPLATE_PATH = '../../!templates/Clipping.md' # location to literature template
 DATE_FORMAT = '%Y%m%d'
+ENCODE_KINDLE = 'utf-8-sig' # native kindle 'My Clippings.txt' encode
+ENCODE = 'utf-8' # unicode encode for new files
 
 
 # create highlights list
@@ -40,13 +41,13 @@ HIGHLIGHTS = []
 
 
 # parse text
-with open(easygui.fileopenbox(), 'r') as file:
+with open(easygui.fileopenbox(), 'r', encoding=ENCODE_KINDLE, errors='ignore') as file:
     list = file.read().split('==========')
     for i in list:
         i = i.split('\n\n') # split i --> [info , quote]
         i[0] = i[0].split('\n- ') # split info --> [title , data]
         data = i[0][1].replace('Note', 'Highlight').split('Highlight on Page ')[1].split(' | ') # split data --> [page , loc , date]
-        title = i[0][0].split('(')[0] # remove author from title
+        title = i[0][0].split('(')[0].strip() # remove author from title
         loc = data[1].split('Loc. ')[1].split('-')[0].strip() # remove prefix and trailing quote
         date = data[2].split('Added on ')[1] # remove prefix quote
         date = datetime.strptime(date, '%A, %B %d, %Y, %I:%M %p') # format date
@@ -58,17 +59,16 @@ with open(easygui.fileopenbox(), 'r') as file:
 # create files
 if not os.path.isdir(CLIPPING_PATH): # check if dir already exists
     os.mkdir(CLIPPING_PATH)
-for i in HIGHLIGHTS:
-    path = CLIPPING_PATH + '/' + CLIPPING_DIR + '-' + i.loc +'.md'
-    if not (os.path.isfile(path)): # check if file already exists
-        copyfile(TEMPLATE_PATH, path)
-        with open(path, 'r') as file:
-            f = file.read()
-            f = f.replace('{{date}}', i.date)
-            f = f.replace('{{title}}', i.title)
-            f = f.replace('{{author}}', CLIPPING_AUTHOR_FIRST + ' ' + CLIPPING_AUTHOR_LAST)
-            f = f.replace('{{year}}', CLIPPING_YEAR)
-            f = f.replace('{{loc}}', i.loc)
-            f = f.replace('{{quote}}', i.quote)
-            with open(path, 'w') as file:
+with open(TEMPLATE_PATH, 'r', encoding=ENCODE) as templateFile:
+    t = templateFile.read()
+    for i in HIGHLIGHTS:
+        path = CLIPPING_PATH + '/' + CLIPPING_DIR + '-' + i.loc +'.md'
+        if not (os.path.isfile(path)): # check if file already exists
+            with open(path, 'w', encoding=ENCODE) as file:
+                f = t.replace('{{date}}', i.date)
+                f = f.replace('{{title}}', i.title)
+                f = f.replace('{{author}}', CLIPPING_AUTHOR_FIRST + ' ' + CLIPPING_AUTHOR_LAST)
+                f = f.replace('{{year}}', CLIPPING_YEAR)
+                f = f.replace('{{loc}}', i.loc)
+                f = f.replace('{{quote}}', i.quote)
                 file.write(f)
