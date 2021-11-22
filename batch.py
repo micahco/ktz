79f7cc2e50849303832a5f73c8e8bd3987aeb9cf
micahco <8213674+micahco@users.noreply.github.com>
2021-11-21 20:22:26 -0800

    win

--- batch.py
+++ batch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import os
 import platform 
-from ktz.helpers import sig, success, error
+from ktz.helpers import sig, success
 
 sig()
 if not platform.system() == 'Windows':
-    error('WARNING', 'Batch file generation only supported on Windows')
+    raise NotImplementedError('Batch file generation only supported on Windows')
 default_path = os.path.expanduser("~/Desktop").replace('/', '\\')
 path = input(f'Batch file location ({default_path}): ') or default_path
 path = os.path.join(path, 'ktz.bat').replace('/', '\\')
